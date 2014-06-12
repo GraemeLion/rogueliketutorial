43b4a2c0484f1843eb136bebac61eb9afc6b0527
Jarrod Henry <jarrodhenry@gmail.com>
2014-06-12 10:47:43 -0500

    Finished with part 2

--- firstrl.py
+++ firstrl.py
@@ -3,6 +3,12 @@ import libtcodpy as libtcod
 SCREEN_WIDTH = 80
 SCREEN_HEIGHT = 50
 LIMIT_FPS = 20
+MAP_WIDTH = 80 
+MAP_HEIGHT = 45 
+
+color_dark_wall = libtcod.Color(0,0,100)
+color_dark_ground = libtcod.Color(50,50,150)
+
 
 libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
 
@@ -14,8 +20,35 @@ con = libtcod.console_new(SCREEN_WIDTH, SCREEN_HEIGHT)
 
 libtcod.sys_set_fps(LIMIT_FPS)
 
-playerx = SCREEN_WIDTH/2
-playery = SCREEN_HEIGHT/2
+class Object:
+    def __init__(self, x, y, char, color):
+        self.x =x 
+        self.y =y
+        self.char = char
+        self.color = color
+
+    def move(self, dx,dy):
+        if not map[self.x+dx][self.y+dy].blocked:
+            self.x += dx;
+            self.y += dy;
+
+    def draw(self):
+        libtcod.console_set_default_foreground(con, self.color)
+        libtcod.console_put_char(con, self.x, self.y, self.char, libtcod.BKGND_NONE)
+
+    def clear(self):
+        libtcod.console_put_char(con, self.x, self.y, ' ', libtcod.BKGND_NONE)
+
+class Tile:
+    def __init__(self, blocked, block_sight = None):
+        self.blocked = blocked
+
+        if block_sight is None: block_sight = blocked
+        self.block_sight = block_sight
+
+player = Object(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, '@', libtcod.white)
+npc = Object(SCREEN_WIDTH/2 - 5, SCREEN_HEIGHT/2, '@', libtcod.yellow)
+objects = [npc, player]
 
 def handle_keys():
     key = libtcod.console_check_for_keypress()
@@ -23,27 +56,59 @@ def handle_keys():
         libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
     elif key.vk == libtcod.KEY_ESCAPE:
         return True
-
-    global playerx, playery
         
     if libtcod.console_is_key_pressed(libtcod.KEY_UP):
-        playery -= 1
+        player.move(0,-1)
     elif libtcod.console_is_key_pressed(libtcod.KEY_DOWN):
-        playery += 1
+        player.move(0,1)
     elif libtcod.console_is_key_pressed(libtcod.KEY_LEFT):
-        playerx -= 1 
+        player.move(-1,0)
     elif libtcod.console_is_key_pressed(libtcod.KEY_RIGHT):
-        playerx += 1
+        player.move(1,0)
 
+            
+def make_map():
+    global map
 
+    map = [[ Tile(False)
+        for y in range(MAP_HEIGHT) ]
+            for x in range(MAP_WIDTH) ]
+
+    map[30][22].blocked = True
+    map[30][22].block_sight = True
+    map[50][22].blocked = True
+    map[50][22].block_sight = True
+
+def render_all():
+    global color_dark_wall
+    global color_dark_ground
+
+    for y in range(MAP_HEIGHT):
+        for x in range(MAP_WIDTH):
+            wall = map[x][y].block_sight
+            if wall:
+                libtcod.console_set_char_background(con,x,y,color_dark_wall, libtcod.BKGND_SET)
+            else:
+                libtcod.console_set_char_background(con,x,y,color_dark_ground, libtcod.BKGND_SET)
+
+    for object in objects:
+        object.draw()
+
+    libtcod.console_blit(con, 0,0,SCREEN_WIDTH,SCREEN_HEIGHT,0,0,0)
+
+make_map()
 
 while not libtcod.console_is_window_closed():
-    libtcod.console_set_default_foreground(con, libtcod.white)
-    libtcod.console_put_char(con,playerx,playery,'@', libtcod.BKGND_NONE)
-    libtcod.console_blit(con,0,0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
+
+    render_all()
     libtcod.console_flush()
 
-    libtcod.console_put_char(con,playerx,playery,' ', libtcod.BKGND_NONE)
+    for object in objects:
+        object.clear()
     exit = handle_keys()
     if exit:
         break 
+
+
+
+
